369a3b696958532fb4ee15b108af61903c75d40c
Shabin-k <helloshabink@gmail.com>
2023-01-29 14:58:07 +0530

    hero section completed

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -6,6 +6,7 @@ import {slideIn,staggerContainer,textContainer,textVariant} from '../utils/motio
 
 const Hero = () => (
   <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+
     <motion.div 
       variants={staggerContainer}
       initial="hidden"
@@ -14,11 +15,13 @@ const Hero = () => (
       className={`${styles.innerWidth} mx-auto flex flex-col`}
       >
         <div className="flex flex-col justify-center items-center relative z-10">
+
           <motion.h1
-          variants={textVariant(1.1)}
-          className={styles.heroHeading}
-          >Metaverse
+           variants={textVariant(1.1)}
+           className={styles.heroHeading}
+           >Metaverse
           </motion.h1>
+
           <motion.div
           variants={textVariant(1.2)}
           className="flex flex-row justify-center items-center"
@@ -27,14 +30,36 @@ const Hero = () => (
             <div className={styles.heroDText}/>
             <h1 className={styles.heroHeading}>Ness</h1>
           </motion.div>
+
         </div>
+
         <motion.div
-        variants={slideIn('Right','tween', 0.2, 1)}
-        className="relative w-full md:-mt-[20px] -mt-[12px]"
-        >
-          <div className=''/>
+          variants={slideIn('right','tween', 0.2, 1)}
+          className="relative w-full md:-mt-[20px] -mt-[12px]"
+         >
+          <div className='absolute w-full  hero-gradient rounded-tl-[140px] z-0 -top-[30px]' />
+
+            <img 
+             src="/cover.png" 
+             alt="cover" 
+             className='w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative'
+            />
+
+            <a href="#explore">
+              <div className='w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10'>
+                <img 
+                  src="/stamp.png" 
+                  alt="stamp"
+                  className='sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain'
+                   />
+              </div>
+            </a>
+
+          
         </motion.div>
+
     </motion.div>
+
   </section>
 );
 
