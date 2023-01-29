5f734a1f95a04a2441b0d41dbba6293064f6f3c9
Shabin-k <helloshabink@gmail.com>
2023-01-29 15:01:23 +0530

    drop some import value

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -2,7 +2,7 @@
 
 import {motion} from 'framer-motion';
 import styles from '../styles'
-import {slideIn,staggerContainer,textContainer,textVariant} from '../utils/motion'
+import {slideIn,staggerContainer,textVariant} from '../utils/motion'
 
 const Hero = () => (
   <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
